a72d70a81f140fec55c452500615642a4eb9ba5c
pacojf93 <127772244+pacojf93@users.noreply.github.com>
2026-03-30 16:47:16 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <p>this is a template for react based projects</p>
+  )
+}
+
+export default App
